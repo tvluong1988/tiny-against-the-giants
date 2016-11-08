3aa537d2fd57fe2e68b18d6adecb9b114461ab7a
Thinh Luong <guitarslacker@gmail.com>
2016-11-07 22:28:34 -0600

    Added terrain map with perlin noise map.

--- Tiny Against the Giants/GameScene.swift
+++ Tiny Against the Giants/GameScene.swift
@@ -21,12 +21,39 @@ class GameScene: SKScene {
   override func sceneDidLoad() {
     
     self.lastUpdateTime = 0
+    self.landBackground = getLandBackground()
+    self.addChild(landBackground)
+  }
+  
+  func getPerlinNoiseMap(frequency: Double, octaveCount: Int = 6, persistence: Double = 0.5, lacunarity: Double = 2.0) -> GKNoiseMap {
+    let seed: Int32 = 10
+    let noiseSource = GKPerlinNoiseSource(frequency: frequency, octaveCount: octaveCount, persistence: persistence, lacunarity: lacunarity, seed: seed)
+    let noise = GKNoise(noiseSource)
+    let noiseMap = GKNoiseMap(noise)
     
-    guard let landBackground = childNode(withName: "grass")
-      as? SKTileMapNode else {
-        fatalError("Background node not loaded")
+    return noiseMap
+  }
+  
+  func getLandBackground() -> SKTileMapNode? {
+    var tileMap: SKTileMapNode?
+    if let tileSet = SKTileSet(named: "BasicTile") {
+      let tileSize = CGSize(width: 32, height: 32)
+      tileMap = SKTileMapNode(tileSet: tileSet, columns: 42, rows: 32, tileSize: tileSize)
+      tileMap?.enableAutomapping = true
+      tileMap?.position = CGPoint.zero
+
+      let noiseMap = getPerlinNoiseMap(frequency: 40)
+      
+      for column in stride(from: 0, to: 42, by: 1) {
+        for row in stride(from: 0, to: 32, by: 1) {
+          if noiseMap.value(at: vector_int2(Int32(column), Int32(row))) > 0.5 {
+            tileMap?.setTileGroup(tileSet.tileGroups.first!, forColumn: column, row: row)
+          }
+        }
+      }
     }
-    self.landBackground = landBackground
+
+    return tileMap
   }
   
   override func update(_ currentTime: TimeInterval) {
